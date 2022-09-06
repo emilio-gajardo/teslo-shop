6faafa978c74cdf174e5634b158af6a6e7074cf2
Emilio Gajardo <ej.gajardo@gmail.com>
2022-09-06 17:56:23 -0300

    Integración de configuraciones para despliegue

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,12 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod' ? true : false,
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+          ? { rejectUnauthorized: false }
+          : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
@@ -24,7 +30,7 @@ import { MessagesWsModule } from './messages-ws/messages-ws.module';
       autoLoadEntities: true,
       synchronize: true
     }),
-    ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'public')}),
+    // ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'public')}),
     ProductsModule,
     CommonModule,
     SeedModule,
